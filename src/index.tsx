d65f04fda0f3b7f8346a1b661b5ce82fadf5e78d
Estevan Maito <ejmaito@gmail.com>
2020-08-26 12:29:20 -0300

    add service worker back

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './tailwind.output.css'
 import Pomodoro from './containers/Pomodoro'
-// import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<Pomodoro />, document.getElementById('root') as HTMLElement)
 
-// serviceWorker.register()
+serviceWorker.register()
